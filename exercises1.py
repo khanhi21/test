2930363ab97db1655044614abc374395812fa988
Khanhi Ramraj <KRamraj@spartaglobal.com>
2020-03-16 16:34:33 +0000

    Scommit

---
+++ exercises1.py
@@ -0,0 +1,46 @@
+# name = input("What is your name?  ")
+# Age = input("how old are you?")
+# DOB = input("what is your DOB? ")
+# Door_Number = input("what is your door number ")
+# First_Line_Address = input("what is your address? ")
+# Postcode = input("what is your postcode? ")
+# print(name, Age, DOB, Door_Number, First_Line_Address, Postcode)
+
+# Palindrome
+# word = "Noon"
+# print(word[::-1])
+# print(word.lower() == word[::-1].lower())
+
+# a = input("Enter your first number ")
+# b = input("Enter your second number ")
+# if (a>b):
+#     print("{} is bigger {} ".format(a, b))
+# else:
+#     print("{} is bigger than {}".format(b, a))
+
+# a = input("Enter your first number ")
+# b = input("Enter your second number ")
+# c = input("Enter your 3rd number ")
+# if(a > b) and (a > c):
+#     print("{} is the largest number".format(a))
+# elif(b > a) and (b > c):
+#     print("{} is the largest number".format(b))
+# else:
+#     print("{} is the largest number".format(c))
+#
+# a = input("Your number pls ")
+# if (int(a) % 2) == 0:
+#     print("your number is even")
+# else:
+#     print("your number is odd")
+
+# a = [1, 2, 5, 7, 8, 3, 19, 27]
+# NewList = []
+#
+# for num in a:
+#     if (num > 15):
+#         NewList.append(num)
+#     else:
+#         continue
+# print(NewList)
+
---
+++ lesson 11.py
@@ -0,0 +1,122 @@
+# print("Hello world")
+# create a variable
+'''
+name = "john"
+print(name)
+a = 1
+b = 2
+c = 3.5
+hi = "hello world"
+print(hi)
+'''
+# print("what is your name?")
+# name = input()
+# print("hi")
+# print(name)
+
+# a = 24
+# b = 16
+# print(a + b)
+# print(a-b)
+# print(4 * "me")
+# print(8%3)
+
+# FloatNum = 1.356
+# IntNum = 4
+# name = "xyz"
+# print(FloatNum + IntNum)
+# print(str(IntNum) + name)
+
+# SingleQuotes = 'look! single quotes'
+# DoubleQuotes = "Look! double quotes"
+# print(SingleQuotes, DoubleQuotes)
+
+# EscapeExample1 = 'I said \'wow\''
+# EscapeExample2 = "I said 'wow'"
+
+# Slicing strings
+# Hw = "Hello world"
+# print(Hw[7:])
+# print(Hw[:5])
+# print(len(Hw))
+
+#String methods
+# WhiteSpace = "lots of spaces at the end             "
+# print(len(WhiteSpace))
+# print(len(WhiteSpace.strip()))
+
+# Count
+# ExampleText = "here is some text with lots of text"
+# print(ExampleText.count("text"))
+# print(ExampleText.upper())
+# print(ExampleText.lower())
+# print(ExampleText.replace("lots", "plenty"))
+
+# Concatenation
+# a = "here "
+# b = "more "
+# c = "much more"
+# d = 10
+# print(a + b + c, d)
+# print(a, b, c, d)
+
+#Concatenation and casting
+
+# x = 2
+# y = 5.4
+# z = "there's now a number 25.4 unless we put a space in"
+# print((str(x)))
+#
+# IntString = "6"
+# print(int(IntString))
+# print(type(int(IntString)))
+# print(float(IntString))
+
+#Booleans
+# a = True
+# # b = False
+# # print(a == b)
+# # print(a != b)
+# # print(a >= b)
+# # print(b <= a)
+
+# hi = "Hello world!"
+# print(hi.isalpha())
+# print(hi.islower())
+# print(hi.isupper())
+# print(hi.endswith("!"))
+# print(hi.startswith("H"))
+
+# Boolean values with numbers
+# x = -1
+# y = 2
+# print(bool(x))
+# print(bool(y))
+# print(bool(None))
+
+#Lists
+# ShoppingList = ["eggs", "bread", "bananas", "biscuits", "milk"]
+# print(type(ShoppingList))
+# print(ShoppingList[0])
+# print(ShoppingList[-1])
+# ShoppingList[1] = "Roti"
+# print(ShoppingList)
+# ShoppingList.pop(-2)
+# print(ShoppingList)
+# print(len(ShoppingList))
+
+#Mixed data type lists
+# mixture = [1, 2, 3, "one", "two", "three"]
+# print(mixture)
+# print(mixture[1:3])
+# print(mixture[-2::])
+
+ListData = [1, 2, 3, 4, 5]
+for m in ListData:
+    print(m*2)
+
+
+
+
+
+
